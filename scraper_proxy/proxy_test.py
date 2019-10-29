3190c342f260f5d0190dfd1dec525fd1ae5353d3
Rafael Nogales <xrnogales@gmail.com>
2019-10-29 17:14:09 +0100

    Add first unit test

--- scraper_proxy/proxy_test.py
+++ scraper_proxy/proxy_test.py
@@ -1,6 +1,14 @@
 from scraper_proxy.proxy import Proxy
+import requests
 
 message = "https://www.amazon.es/Russell-Hobbs-20562-56-SteamGlide-Professional/dp/B00F9SUKE4/"
+
+
+# def test_fixture(requests_mock):
+#     requests_mock.get("http://123-fake-api.com", text="Hello!")
+#     response = requests.get("http://123-fake-api.com")
+#
+#     assert response.text == "Hello!"
 
 
 def test_types():
