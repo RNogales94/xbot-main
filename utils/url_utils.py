909c98d6ab0ed7ea5b961d4ec1aa89a7ae07d056
Rafael Nogales <xrnogales@gmail.com>
2019-12-26 13:01:19 +0100

    activate new user

--- utils/url_utils.py
+++ utils/url_utils.py
@@ -3,6 +3,10 @@ import requests
 import urllib
 
 
+def clean_url(url):
+    return url
+
+
 def capture_urls(text):
     urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
     return urls
